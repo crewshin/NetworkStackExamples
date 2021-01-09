083c5b30903ad3185551930143b327512e1a76b5
Gene Crucean <genecrucean@gmail.com>
2021-01-09 09:23:58 -0500

    Added readme to project for easy viewing inside of xcode

--- NetworkStackExamples/ViewController.swift
+++ NetworkStackExamples/ViewController.swift
@@ -67,7 +67,7 @@ class ViewController: UIViewController {
     func formatHeight(height: Double) -> String {
         let formatter = NumberFormatter()
         formatter.numberStyle = .decimal
-        formatter.maximumFractionDigits = 1
+        formatter.maximumFractionDigits = 2
         return formatter.string(from: NSNumber(value: height)) ?? "ERROR"
     }
 }
